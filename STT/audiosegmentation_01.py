390cf765d545cecbfb3e222c44fbaa6feb9cff58
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-04-17 21:33:40 +0900

    21-04-17 stt

--- STT/audiosegmentation_01.py
+++ STT/audiosegmentation_01.py
@@ -1,3 +1,4 @@
+# https://github.com/jiaaro/pydub/issues/169
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import speech_recognition as sr
@@ -6,13 +7,21 @@ r = sr.Recognizer()
 '''오디오 파일을 불러옴 > silence 부분마다 잘라서 음성 파일 저장 > 해당 파일을 google stt에 적용 > text 출력'''
 
 # 오디오 불러오기
-# sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\F6.wav")
+sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\F6.wav")
+    # 그냥 지켜보며
 # sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\F42.wav")
+    # 동료들도
+    # 저는 나무
 # sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\M14.wav")
+    # 해야 될 거 아니야
 # sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\M15.wav")
+    # 지금은
 # sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\M16.wav")
-# sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\M41.wav")
-sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\predict\\M\\M5.wav") 
+    # 출연자
+    # 조용히 해
+# sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\youtube\\5s\\M41.wav")   
+    # 연기가
+# sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\predict\\M\\M5.wav") 
     # 거리두기 집
     # 자리는 비
 # sound_file = AudioSegment.from_wav("E:\\nmb\\nmb_data\\predict\\M\\M11.wav")
